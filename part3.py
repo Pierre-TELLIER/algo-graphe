f46632cef4d57f9507dc410e0e7cde1aab1d659d
Wiam Arezqi <wiam.arezqi@enseirb-matmeca.fr>
2024-05-10 02:27:06 +0200

    trajet optimal

--- part3.py
+++ part3.py
@@ -2,6 +2,7 @@
 
 #placement des drones dans les villages 
 import random
+import itertools
 
 def position(villages, drones):
     positions = set() 
@@ -61,4 +62,56 @@ def test_degat():
     print("Dégât maximal:", degat_maximal)
 
 #test_degat()
+
+def calculer_temps_de_parcours(tournée, distances):
+    temps = 0
+    for i in range(len(tournée) - 1):
+        village_actuel = tournée[i]
+        village_suivant = tournée[i + 1]
+        temps += distances[(village_actuel, village_suivant)]  
+    return temps
+
+
+#fonction pour le trajet optimal d'une drone
+
+
+def trouver_trajet_optimal(villages, drones, distances):
+    meilleure_tournée = None
+    meilleur_temps = float('inf')
+    
+    permutations = itertools.permutations(villages)
+    
+    for permutation in permutations:
+        temps_total = 0
+        tournées = [permutation[i:i+drones] for i in range(0, len(permutation), drones)]
+        
+        for tournée in tournées:
+            temps_tournée = calculer_temps_de_parcours(tournée, distances)
+            temps_total += temps_tournée
+        
+        if temps_total < meilleur_temps:
+            meilleure_tournée = tournées
+            meilleur_temps = temps_total
+    
+    return meilleure_tournée, meilleur_temps
+
+def test_trajetOptimal() :
+    villages = [1, 2, 3, 4]  
+    drones = 2  
+    distances = {
+        (1, 2): 10, (1, 3): 15, (1, 4): 20,
+        (2, 1): 10, (2, 3): 35, (2, 4): 25,
+        (3, 1): 15, (3, 2): 35, (3, 4): 30,
+        (4, 1): 20, (4, 2): 25, (4, 3): 30
+}  
+
+    meilleure_tournée, meilleur_temps = trouver_trajet_optimal(villages, drones, distances)
+
+    print("Meilleure tournée trouvée :", meilleure_tournée)
+
+    #la meilleure tournée trouvée est [(1, 2), (3, 4)], ce qui signifie que deux drones sont utilisés. Le premier drone parcourt les villages 1 et 2 dans cet ordre, tandis que le deuxième drone parcourt les villages 3 et 4.
+
+    print("Meilleur temps de parcours :", meilleur_temps)
+
+#test_trajetOptimal()
 
