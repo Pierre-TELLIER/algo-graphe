6da34d7f0e4e5da0db072e27e39a20c84f3c3afb
Marie Da Fonseca <mdfonseca001@enseirb-matmeca.fr>
2024-05-11 18:58:25 +0200

    part4

--- part4.py
+++ part4.py
@@ -2,14 +2,14 @@
 
 import heapq
 
-# Importation des classes de votre fichier (imaginaire pour cet exemple)
 from classes import Config, Village, Obstacle, Drone
 
 class Node:
-    def __init__(self, x, y, parent=None):
+    def __init__(self, x, y, village, parent=None):
         self.x = x
         self.y = y
         self.parent = parent
+        self.village = village
         self.f = 0
         self.g = 0
         self.h = 0
@@ -17,15 +17,18 @@ class Node:
     def __lt__(self, other):
         return self.f < other.f
 
-def heuristic(a, b):
-    # Utilise la distance de Manhattan comme heuristique
-    return abs(a.x - b.x) + abs(a.y - b.y)
+def distance(node1, node2):
+    return math.sqrt((node1.x - node2.x)**2 + (node.y - node2.y)**2)
+
+def heuristic(node, destination, water_leaks, time, drones_positions_, visited_villages):
+    distance = distance(node, destination)
+    leaks = calculer_degat_total(drones_positions_, time, visited_villages, water_leaks)
+    return distance + leaks
 
 def a_star(config, start, goal):
     N = config.n
     grid = [[0] * N for _ in range(N)]
-    
-    # Mark obstacles in the grid from config
+
     for o in config.obstacle:
         for x in range(o.x1, o.x2 + 1):
             for y in range(o.y1, o.y2 + 1):
@@ -74,6 +77,27 @@ def a_star(config, start, goal):
                         nodes[(nx, ny)] = neighbor
     
     return []
+
+def plan_path(self, g, start, goal):
+        start_node = Node(start[0], start[1])
+        goal_node = Node(goal[0], goal[1])
+        path = self.a_star_path(g, start_node, goal_node)
+        if path:
+            return [(node.x, node.y) for node in path]
+        else:
+            return []
+
+def update_trajectory(self, g, villages):
+        new_trajet = []
+        for i, village in enumerate(villages):
+            if i == 0:
+                new_trajet.append(village)
+            else:
+                path = self.plan_path(g, new_trajet[-1].pos, village.pos)
+                for pos in path:
+                    new_trajet.append(Village(pos[0], pos[1], 0))
+                new_trajet.append(village)
+        self.trajet = new_trajet
 
 # Example configuration
 obstacles = [Obstacle(3, 3, 5, 5)]
